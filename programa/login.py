528b714b2f7a544faee92503d2f94c0159c986f6
martaip277 <martaip277@gmail.com>
2026-05-12 19:51:02 +0200

    organización del proyecto

---
+++ programa/login.py
@@ -0,0 +1,22 @@
+def cargar_usuarios():
+    usuarios = {}
+    with open("../datos/usuarios.txt", "r", encoding="utf-8") as f:
+        for linea in f:
+            nombre, password = linea.strip().split(",")
+            usuarios[nombre] = password
+    return usuarios
+
+
+def login():
+    print("Inicio de sesión")
+    usuario = input("Usuario: ")
+    password = input("Contraseña: ")
+
+    usuarios = cargar_usuarios()
+
+    if usuario in usuarios and usuarios[usuario] == password:
+        print("Acceso permitido")
+        return True
+    else:
+        print("Usuario o contraseña incorrectos")
+        return False
